7586bad637bb62db51925bb304f17ef30226e1ea
MattreedWest <ligioner86@inbox.ru>
2021-04-17 19:49:07 +0700

    vs

--- main.py
+++ main.py
@@ -1,98 +1,45 @@
 import conf
 import discord
 from discord.ext import commands
+import img_handler as imhl
 
 
 
 # # Настройки INTENTS
-# intense = discord.Intents.default()
-# intense.members = True
-
-# client = discord.Client(intents = intense)
-
-# @client.event
-# async def on_message(message):
-# # <Message 
-# # id=825338226058461184 
-# # channel=<TextChannel id=822806350886207542 name='флудильня' position=0 nsfw=False news=False category_id=822806350886207539>
-# # type=<MessageType.default: 0>
-# # author=<Member id=459312368321691660 name='Twinkle | J4F' discriminator='2408' bot=False nick=None guild=<Guild id=822806350886207538 name='Bots' shard_id=None chunked=False member_count=29>>
-# # flags=<MessageFlags value=0>>
-
-#     if message.author == client.user:
-#         return
-
-#     if message.author.bot:
-#         return
+intentse = discord.Intents.default()
+intentse.members = True
 
 
-#     if message.channel.id == 825339475810582651:
-#         # отправляем сообщение с send
-#         msg = None
+
+bot = commands.Bot(command_prefix = "!", intents = intentse)
 
 
-#         ctx = message.content.split(" ",maxsplit = 1)
+channel = 825328622654193675
 
-#             # 1
-#         if message.content == "/hello":
-#             msg = f'Hello, {message.author.name}. I am {client.user.name}'
+@bot.command(name="get_member")
+async def get_member(ctx, member:discord.Member=None):
+    msg = None
+    global channel
+    if ctx.channel.id == channel:
 
-#             # 2
-#         elif message.content == "/about_me":
-#             msg = f'Hello, {message.author.name}. Your id is {client.user.id}'
+        if member:
+            msg = f'Member {member.name} {"({member.nick})" if member.nick else ""} - {member.id}'
 
-#             # 3
-#         elif ctx[0] ==  "/repeat":
-#             msg = ctx[1]
 
-#             # 4
-#         elif ctx[0] == "/get_member":
-#             if message.author.guild.name == "Bots":
-#                 for member in list(enumerate(message.author.guild.members)):
-#                     if member.id == int(ctx[1]):
-#                         msg += f'He is {member.name}'
-
-#             # 5
-#         elif message.content == "/get_members":
-#             msg = "1"
-#             if message.author.guild.name == "Bots":
-#                 for idx, member in list(enumerate(message.author.guild.members)):
-#                     # "1. name {nick} id"
-#                     msg += f'{idx+1}. {member.name} { f"[{member.nick}]" if member.nick else "" } - {member.id}\n'
-#             # 6
-#         elif message.content == "/get_channels":
-#             msg = " "
-#             if message.author.guild.name == "Bots":
-#                 for idx, channel  in list(enumerate(message.author.guild.channels)):
-#                     msg += f'{idx+1}. {channel.name} - {channel.id}\n'
-#                     print(channel.name)
+        if msg == None:
+            msg = "error"
         
-#             # 7
-#         # elif 
-
-#         if msg != None:
-#             await message.channel.send(msg)
-
-
-# client.run(conf.bot_token)
-
-
-
-
-
-bot = commands.Bot(command_prefix = "!")
-
-
-# args - tuple
-@bot.command(name = "hello")
-async def command_hello(ctx, *args):
-
-    message = " ".join(args)
-    if ctx.channel.id == 825339475810582651:
-        msg = f'Hello to you! You said: "{message}"'
         await ctx.channel.send(msg)
 
+@bot.command(name="mk")
+async def get_member(ctx,f1:discord.Member=None,f2:discord.Member=bot.user):
+    msg = None
+    global channel
+    if ctx.channel.id == channel:
+        if f1 and f2:
+            await imhl.vs_create(f1.avatar_url, f2.abatar_url)
 
+            await ctx.channel.send(file =discord.file(os.path.join("./img/result.png"))
 
 
 bot.run(conf.bot_token)
